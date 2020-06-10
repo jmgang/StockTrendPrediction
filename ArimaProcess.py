2e6dbdc22fa9a20971c05ffdef8be242193f73dc
jansenmarsonang@gmail.com <you@example.com>
2020-06-10 22:01:56 +0800

    Refactored code

--- ArimaProcess.py
+++ ArimaProcess.py
@@ -112,8 +112,8 @@ class ArimaProcess:
         train_x_diff = train_x_diff[n_diff:]
         p, o, q = best_order
 
-        # pre_model = ARIMA(train, order=(p, o, q), exog=exog)
-        # pre_model.fit(disp=0)
+        pre_model = ARIMA(train, order=(p, o, q), exog=exog)
+        pre_model.fit(disp=0)
 
         pre_model = ARIMA(train, order=(p, o, q))
         model_fit = pre_model.fit(disp=0)
@@ -159,14 +159,14 @@ class ArimaProcess:
         print(mse)
 
         indices = self.ftest[2:].index
-        # plt.figure(figsize=(15, 6))
-        # plt.plot(indices, y_pred, color='blue', label='Predicted Price')
-        # plt.plot(indices, test_y, color='red', label='Actual Price')
-        # plt.xlabel('Dates')
-        # plt.ylabel('Prices')
-        # plt.xticks(np.arange(start_index_test, stop_index_test, 100), self.ftest['Date'][::100])
-        # plt.legend()
-        # plt.show()
+        plt.figure(figsize=(15, 6))
+        plt.plot(indices, y_pred, color='blue', label='Predicted Price')
+        plt.plot(indices, test_y, color='red', label='Actual Price')
+        plt.xlabel('Dates')
+        plt.ylabel('Prices')
+        plt.xticks(np.arange(start_index_test, stop_index_test, 100), self.ftest['Date'][::100])
+        plt.legend()
+        plt.show()
 
         pprint(Metrics.forecast_accuracy(y_pred, test_y))
 
